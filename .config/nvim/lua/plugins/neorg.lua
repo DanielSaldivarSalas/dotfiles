7f74b8d02f503bb3c5f5a967c2529284fed5805e
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2024-03-28 03:38:44 -0400

    feat: changed neorg to 8.2.0 from 7

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,24 +1,17 @@
 
 return {
   {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
     "nvim-neorg/neorg",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "laher/neorg-exec" },
-    },
-    --  keys = {
-    -- { "<leader>ne", ":Neorg export to-file ", desc = "Export file" },
-      --    { "<leader>nt", "<cmd>Neorg tangle current-file<cr>", desc = "Tangle file" },
-    --  { "<leader>np", "<cmd>Neorg presenter<cr>", desc = "Presenter" },
-    -- { "<leader>nmi", "<cmd>Neorg inject-metadata<cr>", desc = "Inject" },
-    --  { "<leader>nmu", "<cmd>Neorg update-metadata<cr>", desc = "Update" },
-    -- { "<leader>nol", "<cmd>Neorg toc left<cr>", desc = "Open ToC (left)" },
-    --{ "<leader>nor", "<cmd>Neorg toc right<cr>", desc = "Open ToC (right)" },
-    -- { "<leader>noq", "<cmd>Neorg toc qflist<cr>", desc = "Open ToC (quickfix list)" },
-    --},
-    build = ":Neorg sync-parsers",
+    dependencies = { { "luarocks.nvim" }, { "laher/neorg-exec" } },
+    -- put any other flags you wanted to pass to lazy here!
     config = function()
       require("neorg").setup({
+
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
           ["core.ui"] = {}, -- Loads default behaviour
@@ -50,7 +43,8 @@ return {
               neorg_leader = " ",
               hook = function(keybinds)
                 keybinds.map("norg", "n", "nt", "<cmd>Neorg tangle current-file<CR>")
-              end
+                keybinds.map("norg", "n", "<tab>", "za")
+              end,
             },
             --  config = {
             --   neorg_leader = ",",
@@ -60,3 +54,4 @@ return {
     end,
   },
 }
+   
